d316c079a5f0e31920f03be9a91b2d47fc94214d
shuowu <wushuo2010@gmail.com>
2020-05-06 20:08:26 -0400

    chore: add firefox dev and build script

--- extension/webpack.dev.config.js
+++ extension/webpack.dev.config.js
@@ -1,43 +1,8 @@
-/* eslint-disable */
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
 
-module.exports = {
-  entry: {
-    content: './src/content.js',
-    background: './src/background/index.js',
-    inject: './src/inject/index.js',
-    options: './src/options/index.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
+module.exports = env => merge(common(env), {
   mode: 'development',
   devtool: 'source-map',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  },
-  watch: true,
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: 'node_modules/webextension-polyfill/dist/browser-polyfill.js'
-      }
-    ]),
-    new HtmlWebPackPlugin({
-      template: './src/options/index.html',
-      filename: './options.html',
-      browserPolyfill: './browser-polyfill.js',
-      chunks: ['options']
-    })
-  ]
-}
+  watch: true
+});
