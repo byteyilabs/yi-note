d16d90f86030cd696ff0449ed4c8d845cddddd17
Shuo Wu <wushuo2010@gmail.com>
2020-05-24 11:22:10 -0400

    feat: update installation link (#26)

--- extension/src/constants.js
+++ extension/src/constants.js
@@ -2,8 +2,7 @@ export const APP_ID = 'yi-note';
 export const PAGE = 'page';
 
 export const GITHUB_URL = 'https://github.com/shuowu/yi-note';
-export const INSTALLATION_URL =
-  'https://github.com/shuowu/yi-note#installation';
+export const INSTALLATION_URL = 'https://www.yinote.co#installation';
 
 export const TYPE_BOOKMARKS = 'bookmarks';
 export const TYPE_NOTES = 'notes';
@@ -16,4 +15,7 @@ export const NODE_ENV_PLAYGROUND = 'playground';
 
 export const QUERY_AUTO_JUMP = 'yinotetimestamp';
 
-export const REST_BASE_URL = process.env.NODE_ENV === 'production' ? process.env.REST_BASE_URL_PROD : process.env.REST_BASE_URL_DEV;
+export const REST_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REST_BASE_URL_PROD
+    : process.env.REST_BASE_URL_DEV;
