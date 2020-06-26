9d109ccbaa23fb99f80271784540c64e940b7eb3
Shuo Wu <wushuo2010@gmail.com>
2020-06-26 09:43:56 -0400

    chore: use inline source map in development for chrome debugging

--- extension/webpack.dev.config.js
+++ extension/webpack.dev.config.js
@@ -4,6 +4,6 @@ const common = require('./webpack.common');
 module.exports = env =>
   merge(common(env), {
     mode: 'development',
-    devtool: 'source-map',
+    devtool: 'inline-source-map',
     watch: true
   });
